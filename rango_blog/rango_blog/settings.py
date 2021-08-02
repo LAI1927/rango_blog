f8b28d2a59902469b8a16f0e021d14a8bb330691
LAI1927 <503865526@qq.com>
2021-08-02 20:24:09 +0800

    Database Config

--- rango_blog/rango_blog/settings.py
+++ rango_blog/rango_blog/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = 'rango_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'rangoblog',  # 数据库用户名
+        'PASSWORD': '123456',  # 数据库用户密码
+        'NAME': 'blog'  # 数据库名字
+    },
 }
 
 
